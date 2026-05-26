529265395da1fd43ac92e6e765819e23be203ab4
KU-EMCL <soo4241@korea.ac.kr>
2026-05-26 14:33:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,6 +69,7 @@
       '<div class="bar"><div class="wrap">' +
         '<a class="brand" href="' + abs("index.html") + '">' +
           '<img class="ku-logo" src="' + P + 'images/KU-logo.gif" alt="고려대학교" />' +
+          '<span class="ku-name"><b>고려대학교</b><i>KOREA UNIVERSITY</i></span>' +
           '<span class="mark">EMCL</span>' +
           '<span class="div"></span>' +
           '<span class="meta"><span class="ko">에너지물질순환연구실</span>' +
