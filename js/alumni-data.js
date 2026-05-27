5256f16d5ac2571f55cc1c7525925980fb895c95
KU-EMCL <soo4241@korea.ac.kr>
2026-05-27 20:06:52 +0900

    Add files via upload

---
+++ js/alumni-data.js
@@ -0,0 +1,691 @@
+/* EMCL 졸업생 데이터 — PDF에서 자동 생성. 이미지: images/alumni_XX.jpg */
+window.EMCL_ALUMNI = [
+ {
+  "name": "최형원",
+  "year": "2026",
+  "aff": "삼성전자",
+  "email": "chw000186@korea.ac.kr",
+  "img": "alumni_01.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "신재현",
+  "year": "2026",
+  "aff": "Massachusetts Institute of Technology, MIT 박사과정",
+  "email": "lion9907@korea.ac.kr",
+  "img": "alumni_02.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "신주영",
+  "year": "2026",
+  "aff": "University of California, Berkeley 박사과정",
+  "email": "andrew4747@korea.ac.kr",
+  "img": "alumni_03.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정길",
+  "year": "2026",
+  "aff": "현대자동차",
+  "email": "tltksrkr123@naver.com",
+  "img": "alumni_04.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김성헌",
+  "year": "2026",
+  "aff": "University of California, Berkeley 박사과정",
+  "email": "tjdgjs1745@korea.ac.kr",
+  "img": "alumni_05.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이제형",
+  "year": "2026",
+  "aff": "삼성전자",
+  "email": "dlwpgud9886@korea.ac.kr",
+  "img": "alumni_06.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "윤도성",
+  "year": "2025",
+  "aff": "LG전자",
+  "email": "dozzi5603@korea.ac.kr",
+  "img": "alumni_07.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "송주영",
+  "year": "2024",
+  "aff": "삼성전자",
+  "email": "zoongyoung48@korea.ac.kr",
+  "img": "alumni_08.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "두현진",
+  "year": "2024",
+  "aff": "LG전자",
+  "email": "hyunjin_doo@korea.ac.kr",
+  "img": "alumni_09.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "민재홍",
+  "year": "2024",
+  "aff": "LG전자",
+  "email": "roymin@korea.ac.kr",
+  "img": "alumni_10.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이근정",
+  "year": "2024",
+  "aff": "삼성전자",
+  "email": "orzlvsgg@korea.ac.kr",
+  "img": "alumni_11.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김성곤",
+  "year": "2023",
+  "aff": "성균관대학교 기계공학부 교수",
+  "email": "sgkim14@skku.edu",
+  "img": "alumni_12.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "민하예",
+  "year": "2023",
+  "aff": "SK 이노베이션",
+  "email": "minhaye@korea.ac.kr",
+  "img": "alumni_13.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김대혁",
+  "year": "2023",
+  "aff": "LG전자",
+  "email": "eogurtm@korea.ac.kr",
+  "img": "alumni_14.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정재희",
+  "year": "2023",
+  "aff": "한국조선해양 미래기술연구원(KSOE)",
+  "email": "emcl_jhjeong@korea.ac.kr",
+  "img": "alumni_15.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박세준",
+  "year": "2023",
+  "aff": "LG전자",
+  "email": "bbbaksssae@korea.ac.kr",
+  "img": "alumni_16.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "송민수",
+  "year": "2023",
+  "aff": "LG전자",
+  "email": "qminsoo@korea.ac.kr",
+  "img": "alumni_17.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "오선희",
+  "year": "2022",
+  "aff": "한국냉동공조인증센터",
+  "email": "mdotcpdeltat@korea.ac.kr",
+  "img": "alumni_18.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "정한솔",
+  "year": "2022",
+  "aff": "한국화학연구원",
+  "email": "razorleaf@korea.ac.kr",
+  "img": "alumni_19.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "류승걸",
+  "year": "2022",
+  "aff": "현대제철",
+  "email": "anthony1995@korea.ac.kr",
+  "img": "alumni_20.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박종하",
+  "year": "2022",
+  "aff": "University of California, Berkeley 박사과정",
+  "email": "joshuapjh@korea.ac.kr",
+  "img": "alumni_21.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박준호",
+  "year": "2022",
+  "aff": "삼성전자",
+  "email": "phoko14@korea.ac.kr",
+  "img": "alumni_22.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "임환석",
+  "year": "2021",
+  "aff": "동국제강",
+  "email": "beambitious@korea.ac.kr",
+  "img": "alumni_23.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "Lirong Li",
+  "year": "2021",
+  "aff": "Yangzhou University",
+  "email": "llr_320@163.com",
+  "img": "alumni_24.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "박정준",
+  "year": "2021",
+  "aff": "University of Texas at Austin 박사과정",
+  "email": "danch@korea.ac.kr",
+  "img": "alumni_25.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이가원",
+  "year": "2021",
+  "aff": "삼성전자",
+  "email": "gawon14@korea.ac.kr",
+  "img": "alumni_26.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정재훈",
+  "year": "2020",
+  "aff": "SK 넥실리스",
+  "email": "jaehoon.jung20@sk.com",
+  "img": "alumni_27.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이원혁",
+  "year": "2020",
+  "aff": "University of Wisconsin-Madison 박사과정",
+  "email": "wlee284@wisc.edu",
+  "img": "alumni_28.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김가형",
+  "year": "2020",
+  "aff": "삼성전자",
+  "email": "sun00047@korea.ac.kr",
+  "img": "alumni_29.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김갑용",
+  "year": "2020",
+  "aff": "주 우드워드바이오",
+  "email": "bg@woodwardbio.com",
+  "img": "alumni_30.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "권오진",
+  "year": "2019",
+  "aff": "삼성전자",
+  "email": "samsuny@korea.ac.kr",
+  "img": "alumni_31.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "최성",
+  "year": "2019",
+  "aff": "University of California, Los Angeles 박사과정",
+  "email": "choisung0205@gmail.com",
+  "img": "alumni_32.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이수경",
+  "year": "2018",
+  "aff": "LG전자",
+  "email": "tnrud6917@korea.ac.kr",
+  "img": "alumni_33.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정청우",
+  "year": "2017",
+  "aff": "LG전자",
+  "email": "cwjungemcl@korea.ac.kr",
+  "img": "alumni_34.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "이재원",
+  "year": "2017",
+  "aff": "국립한국해양대학교 기계공학부 교수",
+  "email": "jaewonlee@kmou.ac.kr",
+  "img": "alumni_35.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "Israel Torres Pineda",
+  "year": "2017",
+  "aff": "한국기계연구원(KIMM)",
+  "email": "israeltp@korea.ac.kr",
+  "img": "alumni_36.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "이승주",
+  "year": "2017",
+  "aff": "Texas A&M University 박사과정",
+  "email": "dltmdwn@korea.ac.kr",
+  "img": "alumni_37.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김솔",
+  "year": "2017",
+  "aff": "",
+  "email": "solkim@korea.ac.kr",
+  "img": "alumni_38.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "손병훈",
+  "year": "2017",
+  "aff": "",
+  "email": "guiddurami@korea.ac.kr",
+  "img": "alumni_39.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정명환",
+  "year": "2017",
+  "aff": "Texas A&M University 박사과정",
+  "email": "j16mhwan@kist.re.kr",
+  "img": "alumni_40.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이진기",
+  "year": "2016",
+  "aff": "생산기술연구원(KITECH)",
+  "email": "",
+  "img": "alumni_41.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "양미나",
+  "year": "2016",
+  "aff": "LG전자",
+  "email": "mina.yang@lge.com",
+  "img": "alumni_42.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "Qibei Sun",
+  "year": "2016",
+  "aff": "",
+  "email": "sqb2051@163.com",
+  "img": "alumni_43.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이정훈",
+  "year": "2016",
+  "aff": "LG전자",
+  "email": "junghun01.lee@lge.com",
+  "img": "alumni_44.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이재현",
+  "year": "2016",
+  "aff": "LG화학",
+  "email": "jaehyunlee@lgchem.com",
+  "img": "alumni_45.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김영준",
+  "year": "2015",
+  "aff": "생산기술연구원(KITECH)",
+  "email": "",
+  "img": "alumni_46.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "최재우",
+  "year": "2015",
+  "aff": "두산중공업",
+  "email": "jaewoo4.choi@doosan.com",
+  "img": "alumni_47.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "강문구",
+  "year": "2015",
+  "aff": "LG전자",
+  "email": "mkkang@korea.ac.kr",
+  "img": "alumni_48.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "안승선",
+  "year": "2014",
+  "aff": "SK 매직",
+  "email": "seungsun_an@dayou-winia.com",
+  "img": "alumni_49.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "최익동",
+  "year": "2014",
+  "aff": "두원공조",
+  "email": "",
+  "img": "alumni_50.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김창민",
+  "year": "2014",
+  "aff": "현대제철",
+  "email": "",
+  "img": "alumni_51.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "노구현",
+  "year": "2014",
+  "aff": "두원기술연구소",
+  "email": "",
+  "img": "alumni_52.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "Changwei Pang",
+  "year": "2013",
+  "aff": "Hidragon Capital",
+  "email": "pangchw@gmail.com",
+  "img": "alumni_53.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "김재형",
+  "year": "2013",
+  "aff": "현대자동차",
+  "email": "jaeh@hyundai.com",
+  "img": "alumni_54.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "남성림",
+  "year": "2013",
+  "aff": "현대모비스",
+  "email": "namsr09@hhi.co.kr",
+  "img": "alumni_55.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "조용선",
+  "year": "2013",
+  "aff": "LG전자",
+  "email": "yongsun.jo@lge.com",
+  "img": "alumni_56.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이주혁",
+  "year": "2013",
+  "aff": "경동나비엔",
+  "email": "",
+  "img": "alumni_57.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "최다희",
+  "year": "2013",
+  "aff": "현대건설",
+  "email": "dahee.choi@hdec.co.kr",
+  "img": "alumni_58.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이종성",
+  "year": "2013",
+  "aff": "Toyo 엔지니어링",
+  "email": "jongsung.lee@toyo-eng.com",
+  "img": "alumni_59.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이경열",
+  "year": "2013",
+  "aff": "한국과학기술연구원(KIST)",
+  "email": "ludino@khu.ac.kr",
+  "img": "alumni_60.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김영진",
+  "year": "2011",
+  "aff": "LG전자",
+  "email": "youngjin86.kim@lge.com",
+  "img": "alumni_61.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김응석",
+  "year": "2011",
+  "aff": "한화건설",
+  "email": "",
+  "img": "alumni_62.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이종호",
+  "year": "2011",
+  "aff": "삼성전자",
+  "email": "",
+  "img": "alumni_63.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "설혜연",
+  "year": "2011",
+  "aff": "LG전자",
+  "email": "heayoun.sul@lge.com",
+  "img": "alumni_64.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "Pedro Miguel Agreda …",
+  "year": "2011",
+  "aff": "Universidad de San Carlos de Guatemala",
+  "email": "",
+  "img": "alumni_65.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "조창환",
+  "year": "2010",
+  "aff": "삼성전자",
+  "email": "",
+  "img": "alumni_66.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김현대",
+  "year": "2010",
+  "aff": "한화건설",
+  "email": "lau99@hanhwa.com",
+  "img": "alumni_67.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정종윤",
+  "year": "2010",
+  "aff": "현대자동차",
+  "email": "",
+  "img": "alumni_68.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김범찬",
+  "year": "2009",
+  "aff": "LG전자",
+  "email": "beomchan.kim@lge.com",
+  "img": "alumni_69.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "이강일",
+  "year": "2008",
+  "aff": "대우건설",
+  "email": "kangil.lee@daewooenc.com",
+  "img": "alumni_70.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박중하",
+  "year": "2008",
+  "aff": "현대자동차",
+  "email": "heabysky@hyundai.com",
+  "img": "alumni_71.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김현준",
+  "year": "2007",
+  "aff": "주 만도",
+  "email": "turtle798@naver.com",
+  "img": "alumni_72.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "양승우",
+  "year": "2007",
+  "aff": "삼성SDI",
+  "email": "",
+  "img": "alumni_73.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "조영경",
+  "year": "2006",
+  "aff": "삼성기술연구소",
+  "email": "",
+  "img": "alumni_74.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "정준영",
+  "year": "2006",
+  "aff": "포스코 건설",
+  "email": "",
+  "img": "alumni_75.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김진경",
+  "year": "2005",
+  "aff": "포스코건설",
+  "email": "jkkim@poscoenc.com",
+  "img": "alumni_76.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "김제익",
+  "year": "2005",
+  "aff": "삼성SDI",
+  "email": "jake04.kim@samsung.com",
+  "img": "alumni_77.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "오민규",
+  "year": "2005",
+  "aff": "LG전자",
+  "email": "mingyu.oh@lge.com",
+  "img": "alumni_78.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "차인환",
+  "year": "2005",
+  "aff": "삼성SDI",
+  "email": "",
+  "img": "alumni_79.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박찬형",
+  "year": "2005",
+  "aff": "",
+  "email": "",
+  "img": "alumni_80.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "김성수",
+  "year": "2004",
+  "aff": "우성 산업기술 연구소",
+  "email": "woosung@woosung1983.com",
+  "img": "alumni_81.jpg",
+  "degree": "phd"
+ },
+ {
+  "name": "김정환",
+  "year": "2004",
+  "aff": "Baylor College of Medicine",
+  "email": "junghwan.kim@bcm.edu",
+  "img": "alumni_82.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "윤희정",
+  "year": "2004",
+  "aff": "Atech",
+  "email": "yhj@at1.co.kr",
+  "img": "alumni_83.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "박종익",
+  "year": "2003",
+  "aff": "",
+  "email": "",
+  "img": "",
+  "degree": "ms"
+ },
+ {
+  "name": "조현철",
+  "year": "2003",
+  "aff": "삼성전자",
+  "email": "",
+  "img": "alumni_85.jpg",
+  "degree": "ms"
+ },
+ {
+  "name": "전상현",
+  "year": "2003",
+  "aff": "LG전자",
+  "email": "seamoon1@kebi.com",
+  "img": "",
+  "degree": "ms"
+ }
+];
